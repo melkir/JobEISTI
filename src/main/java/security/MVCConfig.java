98675f983e6ed3c5342c7b0a12b267b27b67e48a
melkir <melkir13@gmail.com>
2015-11-26 21:49:53 +0100

    implement auth in memory

--- src/main/java/security/MVCConfig.java
+++ src/main/java/security/MVCConfig.java
@@ -11,8 +11,8 @@ public class MVCConfig extends WebMvcConfigurerAdapter {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/home").setViewName("home");
         registry.addViewController("/").setViewName("home");
-        registry.addViewController("/hello").setViewName("hello");
         registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/admin").setViewName("admin");
         registry.addViewController("/register").setViewName("register");
     }
 }
