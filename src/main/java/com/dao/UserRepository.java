7b4f1d6b29d61396b56f4d570a8865457edccc26
Thibault Vieux <melkir13@gmail.com>
2015-12-01 04:42:59 +0100

    add user dao crud repo

---
+++ src/main/java/com/dao/UserRepository.java
@@ -0,0 +1,15 @@
+package com.dao;
+
+import com.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    List<User> findByFirstName(String firstName);
+
+    List<User> findByLastName(String lastName);
+
+    List<User> findByEmail(String email);
+}
