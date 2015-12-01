7c96f0baf6b07b82a3ffc21e6a867eecaf65f66d
Thibault Vieux <melkir13@gmail.com>
2015-12-01 04:42:36 +0100

    add user model

---
+++ src/main/java/com/model/User.java
@@ -0,0 +1,34 @@
+package com.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    protected User() {}
+
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%d, firstName='%s', lastName='%s', email='%s']",
+                id, firstName, lastName, email);
+    }
+
+}
+
