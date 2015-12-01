5e76bafcda9255a1a46c34c9b5df615c794e0e24
Thibault Vieux <melkir13@gmail.com>
2015-12-01 04:46:31 +0100

    add directory based permissions

--- src/main/java/com/config/WebSecurityConfig.java
+++ src/main/java/com/config/WebSecurityConfig.java
@@ -10,38 +10,35 @@ import org.springframework.security.config.annotation.web.servlet.configuration.
 @Configuration
 @EnableWebMvcSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        // In memory login - cookie "JSESSIONID"
+        auth.inMemoryAuthentication().withUser("user").password("user").roles("USER");
+        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
+        auth.inMemoryAuthentication().withUser("ent").password("ent").roles("ENTERPRISE");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable(); // temporary disabled for testing file upload
         // Grant resource permissions
         http.authorizeRequests().antMatchers("/webjars/**").permitAll();
         http.authorizeRequests().antMatchers("/css/**").permitAll();
-        // Roles permissions based on directories
-        http.authorizeRequests()
-                .antMatchers("/user/**").access("hasRole('USER')")
-                .antMatchers("/ent/**").access("hasRole('ENTERPRISE')")
-                .antMatchers("/admin/**").access("hasRole('ADMIN')");
-        // Roles permissions for other requests
+        // Roles permissions
         http.authorizeRequests()
                 // All user can access
                 .antMatchers("/", "/home", "/register").permitAll()
-                    .anyRequest().authenticated()
-                    .and()
-                // Other routing need login
-                .formLogin()
-                    .loginPage("/login")
-                    .permitAll()
-                    .and()
+                // Directories access permissions
+                .antMatchers("/user/**").access("hasRole('USER')")
+                .antMatchers("/ent/**").access("hasRole('ENTERPRISE')")
+                .antMatchers("/admin/**").access("hasRole('ADMIN')")
+                // Other requests need the login
+                .anyRequest().authenticated()
+                // Configure the login page
+                .and().formLogin().loginPage("/login").permitAll()
                 // Default logout from Spring Security
-                .logout()
-                    .permitAll();
+                .and().logout().permitAll();
     }
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        // Add 3 login : user, admin and enterprise - cookie "JSESSIONID"
-        auth.inMemoryAuthentication().withUser("user").password("user").roles("USER");
-        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ADMIN");
-        auth.inMemoryAuthentication().withUser("ent").password("ent").roles("ENTERPRISE");
-    }
 }
