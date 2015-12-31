9fa2618712f2f63f031f25c5cf248811b63a78b6
melkir <melkir13@gmail.com>
2015-12-31 17:00:21 +0100

    Implement H2 Database, H2 console and basic user auth without roles part 2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user(email,password,first_name,last_name) values ('user@example.com','user','Rob','Winch');
+insert into user(email,password,first_name,last_name) values ('admin@example.com','admin','Luke','Taylor');
+insert into user(email,password,first_name,last_name) values ('ent@example.com','ent','John','Cena');
