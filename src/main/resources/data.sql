fb969d11272382cdb6c7659c497052377d00049c
Thibault Vieux <melkir13@gmail.com>
2016-01-03 14:53:26 +0100

    ent to enterprise login

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,6 @@
 INSERT INTO user(email,password,username,first_name,last_name) VALUES ('user@mail.com','user','user','Rob','Winch');
 INSERT INTO user(email,password,username,first_name,last_name) VALUES ('admin@mail.com','admin','admin','Luke','Taylor');
-INSERT INTO user(email,password,username,first_name,last_name) VALUES ('ent@mail.com','ent','ent','John','Cena');
+INSERT INTO user(email,password,username,first_name,last_name) VALUES ('ent@mail.com','enterprise','enterprise','John','Cena');
 INSERT INTO user(email,password,username,first_name,last_name) VALUES ('dba@mail.com','dba','dba','Luck','Skywalker');
 INSERT INTO USER_ROLE(role, username) VALUES ('USER', 1);
 INSERT INTO USER_ROLE(role, username) VALUES ('ADMIN', 2);
