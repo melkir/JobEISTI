e32e3878ac7b53b68b6f817249ecd0b1e37800f5
melkir <melkir13@gmail.com>
2016-01-05 18:04:32 +0100

    fix typo

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO user(email,password,username,first_name,last_name) VALUES ('user@mail.com','user','user','Rob','Winch');
 INSERT INTO user(email,password,username,first_name,last_name) VALUES ('admin@mail.com','admin','admin','Luke','Taylor');
 INSERT INTO user(email,password,username,first_name,last_name) VALUES ('ent@mail.com','enterprise','enterprise','John','Cena');
-INSERT INTO user(email,password,username,first_name,last_name) VALUES ('dba@mail.com','dba','dba','Luck','Skywalker');
+INSERT INTO user(email,password,username,first_name,last_name) VALUES ('dba@mail.com','dba','dba','Luke','Skywalker');
 INSERT INTO USER_ROLE(role, username) VALUES ('USER', 1);
 INSERT INTO USER_ROLE(role, username) VALUES ('ADMIN', 2);
 INSERT INTO USER_ROLE(role, username) VALUES ('ENTERPRISE', 3);
